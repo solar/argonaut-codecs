e7e1c9182b0077fc12bec8aaec75d3355505ec7e
solar <github@sazabi.org>
2015-03-20 00:46:52 +0900

    create CodecJson from A => B and B => String \/ A

--- core/src/main/scala/org/sazabi/argonaut/codecs/Disjunction.scala
+++ core/src/main/scala/org/sazabi/argonaut/codecs/Disjunction.scala
@@ -7,4 +7,13 @@ object Disjunction {
   def decoder[A](f: Json => String \/ A): DecodeJson[A] = DecodeJson { a =>
     f(a.focus).fold(DecodeResult.fail(_, a.history), DecodeResult.ok)
   }
+
+  def codec[A, B](to: A => B, from: B => String \/ A)(implicit e: EncodeJson[B], d: DecodeJson[B]): CodecJson[A] =
+    CodecJson(a => e.encode(to(a)), { a =>
+      for {
+        b <- d.decode(a)
+        r <- from(b).fold(str => DecodeResult.fail(str, a.history),
+          DecodeResult.ok(_))
+      } yield r
+    })
 }
